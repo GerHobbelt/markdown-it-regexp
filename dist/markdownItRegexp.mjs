b7dc6b7aa54ddc7e2bd0e274855ba2e43028972c
Ger Hobbelt <ger@hobbelt.com>
2020-06-17 20:51:27 +0200

    bumped build revision

--- dist/markdownItRegexp.mjs
+++ dist/markdownItRegexp.mjs
@@ -1,4 +1,4 @@
-/*! markdown-it-regexp 0.5.0-3 https://github.com//GerHobbelt/markdown-it-regexp @license MIT */
+/*! markdown-it-regexp 0.5.0-4 https://github.com//GerHobbelt/markdown-it-regexp @license MIT */
 
 /*!
  * markdown-it-regexp
